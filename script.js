6a77945dd94236909ba27c3456a0341212a4a623
Komal Gill <komalgill0310@gmail.com>
2022-02-27 10:12:59 -0500

    changed functions name from createSnake & createApple TO drawSnake & drawApple

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ function fillCanvas(){
   canvasContext.fillRect(0,0,canvas.width,canvas.height);
 } 
 
-function createSnake(){
+function drawSnake(){
   canvasContext.fillStyle = "green";
   canvasContext.fillRect(350,250,29,19);
 }
 
-function createApple(){
+function drawApple(){
   canvasContext.fillStyle = "red";
   canvasContext.arc(150,105,5,0, 2*Math.PI);
   canvasContext.fill();
 }
 
 fillCanvas();
-createSnake();
-createApple();
+drawSnake();
+drawApple();
