14da9171763260917245be9bc80ee7f61fde7829
Komal Gill <komalgill0310@gmail.com>
2022-02-27 09:05:51 -0500

    created snake and apple

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 let canvas = document.getElementById("snake-game-canvas");
 let canvasContext = canvas.getContext("2d");
-canvasContext.fillStyle = "black";
-canvasContext.fillRect(0,0,600,500);
+
+function fillCanvas(){
+  canvasContext.fillStyle = "black";
+  canvasContext.fillRect(0,0,canvas.width,canvas.height);
+} 
+
+function createSnake(){
+  canvasContext.fillStyle = "green";
+  canvasContext.fillRect(350,250,29,19);
+}
+
+function createApple(){
+  canvasContext.fillStyle = "red";
+  canvasContext.arc(150,105,5,0, 2*Math.PI);
+  canvasContext.fill();
+}
+
+fillCanvas();
+createSnake();
+createApple();
