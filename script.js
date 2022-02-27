f9bdbb7d3e9462a90685961fac67b30e7ba7fb3a
Komal Gill <komalgill0310@gmail.com>
2022-02-27 10:42:23 -0500

    snake is moving in canvas

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 let canvas = document.getElementById("snake-game-canvas");
 let canvasContext = canvas.getContext("2d");
+let moveSnake = 5;
+// fillCanvas();
+setInterval(drawSnake,1000);
+drawSnake();
+// drawApple();
 
-function fillCanvas(){
-  canvasContext.fillStyle = "black";
-  canvasContext.fillRect(0,0,canvas.width,canvas.height);
-} 
+
+// function fillCanvas(){
+//   canvasContext.fillStyle = "black";
+//   canvasContext.fillRect(0,0,canvas.width,canvas.height);
+// } 
 
 function drawSnake(){
+  canvasContext.fillStyle = "black";
+  canvasContext.fillRect(0,0,canvas.width,canvas.height);
+  moveSnake = moveSnake + 8;
   canvasContext.fillStyle = "green";
-  canvasContext.fillRect(350,250,29,19);
-}
-
-function drawApple(){
+  canvasContext.fillRect(moveSnake,250,29,19);
   canvasContext.fillStyle = "red";
   canvasContext.arc(150,105,5,0, 2*Math.PI);
   canvasContext.fill();
 }
 
-fillCanvas();
-drawSnake();
-drawApple();
+// function drawApple(){
+//   canvasContext.fillStyle = "black";
+//   canvasContext.fillRect(0,0,canvas.width,canvas.height);
+//   canvasContext.fillStyle = "red";
+//   canvasContext.arc(150,105,5,0, 2*Math.PI);
+//   canvasContext.fill();
+// }
+
